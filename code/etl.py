f067b01057081532619fd0e78443ff45e2d39555
Rodolfo Stremel <rodolfostremel@gmail.com>
2024-06-27 17:34:23 -0300

    clean_data and log_files folders created

--- code/etl.py
+++ code/etl.py
@@ -3,16 +3,16 @@ import pandas as pd
 from decorators import log_decorator, cronometro
 import glob
 import os
-
-logger.add("file_{time}.log", format="{time} {level} {message}", level="INFO")
+# C:\Users\rodol\Documents\data_eng_projetos\testando-logs-e-decoradores\log_files
+logger.add("./log_files/file_{time}.log", format="{time} {level} {message}", level="INFO")
 
 @cronometro
 def extrair_dados(pasta: str) -> pd.DataFrame:
     arquivos_json = glob.glob(os.path.join(pasta, '*.json'))
     df_list = [pd.read_json(arquivo) for arquivo in arquivos_json]
     df_total = pd.concat(df_list, ignore_index=True)
-    print(extrair_dados.__name__)  # Saída: funcao_demorada
-    print(extrair_dados.__doc__)   # Saída: Esta função simula um processamento demorado.
+    print(extrair_dados.__name__)
+    print(extrair_dados.__doc__)
     return df_total
 
 @cronometro
@@ -24,7 +24,7 @@ def calcular_kpi_de_total_de_vendas(df: pd.DataFrame) -> pd.DataFrame:
 def salvar_arquivo_novo_de_dados(df: pd.DataFrame, formato_saida: list):
     for formato in formato_saida:
         if formato == "csv":
-            df.to_csv("dados.csv", index=False)
+            df.to_csv("./clean_data/dados.csv", index=False)
         if formato == "parquet":
             df.to_parquet("dados.parquet", index=False)
 
